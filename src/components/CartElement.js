e93fb0bfd7c8b0367e3df9a7084a2ffccb92f640
Benoît Vicente <benoit.vicente@hotmail.fr>
2023-12-20 10:53:13 +0100

    première version fonctionelle des boutons + - et supprimer du cart

--- src/components/CartElement.js
+++ src/components/CartElement.js
@@ -8,9 +8,34 @@ function CartElement({name, price, amount, cart, updateCart}){
         setIsShown(false)
         updateCart(cart.filter((plant) => plant.name !== name))
     }
+    function addToCart(){
+        const cartFilteredCurrentPlant = cart.filter(
+            (plant) => plant.name !== name
+        )
+        updateCart([
+            ...cartFilteredCurrentPlant,
+            { name, price, amount: amount + 1 }])
+    }
+
+    function removerFromCart()
+    {
+        if(amount === 1){
+            deleteLine()
+        } else {
+            const cartFilteredCurrentPlant = cart.filter(
+                (plant) => plant.name !== name
+            )
+            updateCart([
+                ...cartFilteredCurrentPlant,
+                { name, price, amount: amount - 1 }])
+        }
+    }
+
     return (
         isShown && ( <div>
 			            {name} {price}€ x {amount} <br/>
+                        <button onClick={() => addToCart()}>➕</button>
+                        <button onClick={() => removerFromCart()}>➖</button>
                          <button onClick={() => deleteLine()}>🗑️Supprimer</button>
 		            </div>
                     )   
